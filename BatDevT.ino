2c44bbd2b07c9c0f9921d89ec55de33c3bc30cd4
Mike <MichaelSRaven@gmail.com>
2015-12-20 13:46:17 -0800

    Locale, new Loads functions, miscellaneous tweaks

--- BatDevT.ino
+++ BatDevT.ino
@@ -7,7 +7,7 @@
 //
 //    Version 4.0: Port previous application to new drivers
 //
-//    Version 4.1: Changed Duemilanove bootloader to OptiBoot, gaining ~1500 bytes
+//    Version 4.1: Changed Duemilanove bootloader to OptiBoot, gaining 1500 bytes
 //
 //    Version 4.2: Inaugurate use of new virtual timer package
 //
@@ -18,11 +18,12 @@
 #include <EEPROM.h>
 #include <DallasTemperature.h>
 #include <Utility.h>
+#include <BatDevLocale.h>
 #include "Drivers.h"
 #include "DriverTimer.h"
 #include "Smoothing.h"
 
-#undef Calibrate                   // Do NOT include calibration code
+#undef  Calibrate                  // Do NOT include calibration code
 
 #define MAmpCeiling   3000         // Max allowable thru INA219 shunt resistor
 #define MaxBatTemp    40.9         // Max allowed battery temperature
@@ -30,7 +31,8 @@
 #define ChargeTemp    8.0          // Charge is complete if delta T exceeds 8.0 deg C
 
 
-//--------------Record types--------------
+//-------------- Record types -------------
+
 #define typeCC         0     // CTRecord format: shuntMA, busV, thermLoad, thermAmbient, millisecs
 #define typeCV         1     // Use CTRecord format
 #define typeDetect     2     // Dip Detected; CTRecord format
@@ -44,29 +46,25 @@
 #define typeNudge     13     // nudgeCount, potLevel, millis
 #define typeIRes      14     // Internal Resistance record, ohms
 
-#define BandPlus  7.0              // ConstantCurrent upper band width
-#define BandMinus 3.0              // ConstantCurrent lower band width
+#define BandPlus     7.0     // ConstantCurrent upper band width
+#define BandMinus    3.0     // ConstantCurrent lower band width
 
 
-//-------------globals-------------
+//---------------- Globals ----------------
 
 char battID[20] = "<undefined>";
 int capacity = 2400;                      // Battery capacity in mAh
-
-
-boolean scriptrunning = false;            // Regulates issuance of '~' to close external records file
 
 typedef struct DispatchTable {
     const char *command;                  // Command name
     exitStatus (*handler)(char **);       // Pointer to command handler
 };
 
-
 const struct DispatchTable commandTable[] = {
-    { "gauss",      TestGauss       },             // <<< TESTING Gaussian smoothing >>>
     { "b",          SetID           },
     { "bc",         SetCapacity     },
     { "bp",         BatPresentCmd   },
+    { "bt",         BatteryTypeCmd  },
     { "cc",         ccCmd           },
     { "cv",         cvCmd           },
     { "d",          DischargeCmd    },
@@ -74,7 +72,7 @@ const struct DispatchTable commandTable[] = {
     { "heat",       ReportHeats     },
     { "help",       PrintHelp       },
     { "iget",       iGetCmd         },
-    { "loff",       LoffCmd         },  // Load off
+    { "loff",       LoffCmd         },
     { "lon",        LonCmd          },  // Load on; args h, m, l, b [hi, med, lo, bus]
 //  { "jugs",       ReportJugs      },
     { "nudge",      NudgeCmd        },
@@ -88,7 +86,8 @@ const struct DispatchTable commandTable[] = {
     { "script",     ScriptCmd       },
     { "setpga",     PgaCmd          },
     { "tell",       Report          },
-    { "thermo",     ThermLoop       },  // Access to 'ThermMonitor'
+    { "thermo",     ThermLoop       },
+    { "ver",        VersionCmd      },
     { "vget",       vGetCmd         },
     { "vset",       VsetCmd         },
     {  NULL,        unknownCommand  }   // Insert additional commands BEFORE this line
@@ -115,18 +114,20 @@ void setup (void)
     InitLoads();
     SetPGA(8);
 
-    FreeRam(NULL);  // Keep tabs on amount of free RAM.
-    SetID(NULL);    // Print the default battery ID as a
-                    // ...reminder to set the actual one.
+    VersionCmd(NULL);
+    FreeRam(NULL);      // Keep tabs on amount of free RAM.
+    SetID(NULL);        // Print the default battery ID as a
+                        // ...reminder to set the actual one.
 }
 
 
 void loop (void)
 {
     int i;
-    exitStatus rc;
     char **t;
+    exitStatus rc;
 
+    Printf("> ");
     t = util.GetCommand();
     if (*t == NULL)
         return;
@@ -135,8 +136,12 @@ void loop (void)
             break;
 
     rc = (*commandTable[i].handler)(t);    // call command handler
+
     if (rc != 0) {
         ReportExitStatus(rc);
-        Printf("~\n");
+		Printf("\n");
     }
+    if (strcmp(commandTable[i].command, "script") == 0)     // <<< EVALUATING >>>
+        Printf("~");
+
 }
