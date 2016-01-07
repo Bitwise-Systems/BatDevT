870c5720e4603921f48448f8e9eb9e308b5156e1
Mike <MichaelSRaven@gmail.com>
2016-01-07 11:49:23 -0800

    Loadable scripts, iterative compiler, private memory pool

--- Script.ino
+++ Script.ino
@@ -1,92 +1,89 @@
 //---------------------------------------------------------------------------------------
-//      Script.ino  --  Script compiler & interpreter
+//      Script.ino -- Script processor
 //---------------------------------------------------------------------------------------
 
-char *commandText = NULL;           // Command text buffer
-const int AllocSize = 200;          // Initial size for buffer allocation
-int bufsize = 0;                    // Actual buffer size after truncation
+#include <setjmp.h>
+
+jmp_buf environment;    // A non-local jump is used to recover from 'alloc' errors.
+
+struct code {                          // Linked list node structure
+    struct code *next;
+    char *text;
+} head = {NULL, NULL}, *tail = &head;  // List management variables
 
 
-#define MaxCommandsPerScript 15     // Maximum script length (in lines)
-#define MaxTokensPerCommand 4       // Command name plus three arguments
-
-static char *tokptr[MaxCommandsPerScript + 1][MaxTokensPerCommand + 1];
+static char *parmList[MaxTokens + 1];  // Traditional argument list passed to commands
 
 
-exitStatus CompileCmd (char **args)    // Compile a script
+//---------------------------------------------------------------------------------------
+//      CompileCmd -- Compile a script presented on the serial input stream
+//---------------------------------------------------------------------------------------
+
+exitStatus CompileCmd (char **args)
 {
-    int i, j;
-    char **t, *bufptr;
-    int bytesToCopy, bytesAvail = AllocSize;
+    char **t;
 
-    if (commandText != NULL)
-        free(commandText);
-
-    bufptr = commandText = (char *) malloc(AllocSize);
-    if (commandText == NULL) {
-        abortCompile("can't allocate memory");
+    if (setjmp(environment) != 0) {    // Bounce back here on alloc error
+        handleOverflow();
         return ParameterError;
     }
-    for (i = 0; *(t = util.GetCommand()) != NULL; i++) {
-        j = 0;
-        do {
-            if (i >= MaxCommandsPerScript) {
-                abortCompile("too many commands");
-                return ParameterError;
-            }
-            if (j >= MaxTokensPerCommand) {
-                abortCompile("too many tokens");
-                return ParameterError;
-            }
-            if ((bytesToCopy = strlen(*t) + 1) > bytesAvail) {
-                abortCompile("text buffer overflow");
-                return ParameterError;
-            }
-            tokptr[i][j++] = bufptr;
-            strncpy(bufptr, *t, bytesToCopy);
-            bufptr += bytesToCopy;
-            bytesAvail -= bytesToCopy;
+    freeScriptMemory();
 
+    while (*(t = util.GetCommand()) != NULL) {    // Blank line terminates compilation
+        do {
+            tail = append(tail, *t);
         } while (*++t != NULL);
-        tokptr[i][j] = NULL;
+
+        tail = append(tail, NULL);
     }
-    tokptr[i][0] = NULL;
-    bufsize = bufptr - commandText;
-    commandText = (char *) realloc(commandText, bufsize);
+    return Success;
+
+}
+
+
+//---------------------------------------------------------------------------------------
+//      ListCmd -- List out the contents of the currently compiled script
+//---------------------------------------------------------------------------------------
+
+exitStatus ListCmd (char **args)
+{
+    struct code *p;
+
+    for (p = head.next; p != NULL; p = p->next)
+        if (p->text == NULL)
+            Printf("\n");
+        else
+            Printf("%s ", p->text);
 
     return Success;
 
 }
 
 
-static void abortCompile (char *errorMessage)    // Recover from compiler errors
+//---------------------------------------------------------------------------------------
+//      RunCmd -- Run the current script
+//---------------------------------------------------------------------------------------
+
+exitStatus RunCmd (char **args)
 {
-    Printf("Script compiler error: %s\n", errorMessage);
-    if (commandText != NULL) {
-        free(commandText);
-        commandText = NULL;
-    }
-    bufsize = 0;
-    memset(tokptr, 0, sizeof tokptr);
-
-}
-
-
-exitStatus RunCmd (char **args)      // Script command processor
-{
-    int i, j;
+    int i;
+    char *name;
     exitStatus rc;
-    static exitStatus goodRC[] = {Success, DipDetected, MaxTime, ChargeTempThreshold, SENTINEL};
+    struct code *p;
 
-    for (j = 0; tokptr[j][0] != NULL; j++) {
+    static exitStatus goodRC[] =
+        {Success, DipDetected, MaxTime, ChargeTempThreshold, SENTINEL};
+
+    for (p = head.next; p != NULL; p = p->next) {
         for (i = 0; commandTable[i].command != NULL; i++)
-            if (strcmp(commandTable[i].command, *tokptr[j]) == 0)
+            if (strcmp(commandTable[i].command, name = p->text) == 0)
                 break;
 
-        rc = (*commandTable[i].handler)(tokptr[j]);
+        p = buildParmList(p);
+        rc = (*commandTable[i].handler)(parmList);
 
         if (! memberQ(rc, goodRC)) {
-            Printf("Untoward return code from '%s' (rc = %d), ", *tokptr[j], rc);
+            Printf("Untoward return code from '%s' (rc = %d) ", name, rc);
             return rc;
         }
     }
@@ -95,7 +92,37 @@ exitStatus RunCmd (char **args)      // Script command processor
 }
 
 
-static boolean memberQ (exitStatus element, exitStatus *set)    // Set membership query
+//---------------------------------------------------------------------------------------
+//      Private routines...
+//---------------------------------------------------------------------------------------
+
+static struct code *append (struct code *tail, char *str)
+{
+    struct code *node = (struct code *) alloc(sizeof(struct code));
+
+    node->next = NULL;
+    node->text = NULL;
+    tail->next = node;
+    if (str != NULL)
+        node->text = saveString(str);    // 'alloc' failure possible here too
+    return node;
+
+}
+
+
+static struct code *buildParmList (struct code *p)
+{
+    memset(parmList, 0, sizeof(parmList));
+
+    for (int i = 0; p->text != NULL; (i++, p = p->next))
+        parmList[i] = p->text;
+
+    return p;    // Output list size can't exceed input, therefore will fit.
+
+}
+
+
+static boolean memberQ (exitStatus element, exitStatus *set)
 {
     for (int i = 0; set[i] != SENTINEL; i++)
         if (element == set[i])
@@ -106,14 +133,62 @@ static boolean memberQ (exitStatus element, exitStatus *set)    // Set membershi
 }
 
 
-exitStatus ListScriptCmd (char **args)    // List script contents
+static char *saveString (char *s)
 {
-    for (int i = 0; tokptr[i][0] != NULL; i++) {
-        Printf(":   ");
-        for (int j = 0; tokptr[i][j] != NULL; j++)
-            Printf("%s ", tokptr[i][j]);
-        Printf("\n");
+    char *p = alloc(strlen(s) + 1);
+    strcpy(p, s);
+    return p;
+}
+
+
+static void handleOverflow (void)
+{
+    freeScriptMemory();                             // No partial scripts
+    Printf("Out of memory in script compiler: ");
+
+    delay(50);                          // Let rest of script flow
+    while (Serial.available()) {        // ...in, and then discard it.
+        (void) Serial.read();
+        delay(50);
     }
+}
+
+
+//---------------------------------------------------------------------------------------
+//      Dynamic memory manager, used exclusively by 'append' & 'saveString'
+//---------------------------------------------------------------------------------------
+
+#define PoolSize 600
+
+static char memoryPool[PoolSize];       // fixed size memory pool
+static char *allocp = memoryPool;       // next free position
+
+static char *alloc (int n)
+{
+    if (memoryPool + PoolSize - allocp < n)    // Fatal error: out of memory
+        longjmp(environment, 1);
+    else {
+        allocp += n;
+        return allocp - n;
+    }
+}
+
+
+static void freeScriptMemory (void)
+{
+    allocp = memoryPool;
+    head.next = NULL;
+    tail = &head;
+
+}
+
+
+exitStatus MemQCmd (char **args)        // <<< TESTING >>>
+{
+    int used = allocp - memoryPool;
+    int avail = PoolSize - used;
+    Printf("In use: %d bytes. Available: %d bytes.\n", used, avail);
+
     return Success;
 
 }
